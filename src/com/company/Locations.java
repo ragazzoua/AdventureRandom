e64d7ee68d6f66770fa4d8332f3c297b62cae6d4
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-07-08 13:39:28 +0300

    new branch created Byte Streams

--- src/com/company/Locations.java
+++ src/com/company/Locations.java
@@ -7,16 +7,8 @@ public class Locations implements Map<Integer, Location> {
     private static Map<Integer, Location> locations = new LinkedHashMap<>();
 
     public static void main(String[] args) throws IOException {
-        try (BufferedWriter locFile = new BufferedWriter(new FileWriter("locations.txt"));
-             BufferedWriter dirFile = new BufferedWriter(new FileWriter("directions.txt"))) {
-            for (Location location : locations.values()) {
-                locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
-                for (String direction : location.getExits().keySet()) {
-                    if (!direction.equalsIgnoreCase("Q")) {
-                        dirFile.write(location.getLocationID() + "," + direction + "," + location.getExits().get(direction) + "\n");
-                    }
-                }
-            }
+        try {
+            DataOutputStream locfile = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("locations.dat")));
         }
     }
 
